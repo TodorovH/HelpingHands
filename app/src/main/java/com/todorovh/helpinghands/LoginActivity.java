c651fc8fab0e7e270499cbb55088aa5498988ab1
TodorovH <hristo_petkov2001@abv.bg>
2015-12-21 14:24:02 +0200

    new commit

--- app/src/main/java/com/todorovh/helpinghands/LoginActivity.java
+++ app/src/main/java/com/todorovh/helpinghands/LoginActivity.java
@@ -1,12 +1,45 @@
 package com.todorovh.helpinghands;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText username;
+    private EditText password;
+    private Button bLogin;
+    private TextView registerLink;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        init();
+        bLogin.setOnClickListener(this);
+        registerLink.setOnClickListener(this);
+    }
+
+    private void init(){
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        bLogin = (Button) findViewById(R.id.button_login);
+        registerLink = (TextView) findViewById(R.id.registerLink);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_login:
+                //TODO...
+                break;
+            case R.id.registerLink:
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                break;
+        }
     }
 }
--- app/src/main/java/com/todorovh/helpinghands/RegisterActivity.java
+++ app/src/main/java/com/todorovh/helpinghands/RegisterActivity.java
@@ -1,14 +1,59 @@
 package com.todorovh.helpinghands;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText name;
+    private EditText age;
+    private EditText city;
+    private EditText address;
+    private EditText phone;
+    private EditText email;
+    private EditText rUsername;
+    private EditText rPassword;
+    private EditText rPasswordConfirm;
+    private Button bRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        init();
+        bRegister.setOnClickListener(this);
     }
 
+    private void init(){
+        name = (EditText) findViewById(R.id.register_name_text);
+        age = (EditText) findViewById(R.id.register_age_text);
+        city = (EditText) findViewById(R.id.register_city_text);
+        address = (EditText) findViewById(R.id.register_address_text);
+        phone = (EditText) findViewById(R.id.register_phone_text);
+        email = (EditText) findViewById(R.id.register_email_text);
+        rUsername = (EditText) findViewById(R.id.register_username_text);
+        rPassword = (EditText) findViewById(R.id.register_password_text);
+        rPasswordConfirm = (EditText) findViewById(R.id.register_password_confirm_text);
+        bRegister = (Button) findViewById(R.id.button_register);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_register:
+
+
+
+                //bRegister.setBackgroundColor(Color.WHITE);
+                //bRegister.setTextColor(Color.BLUE);
+                //TODO...
+                break;
+        }
+    }
 }
